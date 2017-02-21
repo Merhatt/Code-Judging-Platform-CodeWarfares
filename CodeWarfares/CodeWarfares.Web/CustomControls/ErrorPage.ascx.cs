105095804c2b52aa98e251690ed896517dc89193
Merhatt <merhat1998@gmail.com>
2017-02-21 16:13:21 +0200

    Error handlers added

---
+++ CodeWarfares/CodeWarfares.Web/CustomControls/ErrorPage.ascx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CodeWarfares.Web.CustomControls
+{
+    public partial class ErrorPage : System.Web.UI.UserControl
+    {
+        private string errorCode;
+
+        public string ErrorCode
+        {
+            get
+            {
+                return this.errorCode;
+            }
+            set
+            {
+                this.errorCode = value;
+                this.ErrorCodeValue.Text = value;
+            }
+        }
+    }
+}
